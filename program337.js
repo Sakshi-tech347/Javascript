d9e170286b13c1311ea263245ebf412eb2ec42c9
sakshi kachate <sakshikachate347@gmail.com>
2026-05-20 21:59:58 +0530

    minmax(number)

---
+++ program337.js
@@ -0,0 +1,17 @@
+
+function main()
+{
+
+   let iNo1 = 11 , iNo2 = 21;
+
+   if (iNo1 > iNo2)
+   {
+      console.log(" no1 is MAX");
+   }
+   else
+   {
+      console.log("no2 is MAX");
+   }
+}
+
+main();
