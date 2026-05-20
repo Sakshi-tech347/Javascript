c64559ad0cdc9dedac1dce74425f3b26363a7a96
sakshi kachate <sakshikachate347@gmail.com>
2026-05-21 00:14:23 +0530

    Addition

---
+++ program355.js
@@ -0,0 +1,18 @@
+ 
+Addition=(no1,no2) => {return no1+no2};
+
+/*function Addition(no1 , no2)
+{
+
+ return no1 + no2;
+}*/
+
+function main()
+{
+    let iRet = 0;
+    iRet =Addition(11 ,21);
+    console.log("Addition is :"+iRet);
+
+}
+
+main();
