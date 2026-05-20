81f485445843f6a319f560fe789adaaa7a11691c
sakshi kachate <sakshikachate347@gmail.com>
2026-05-21 00:16:33 +0530

    intro Dictionary

---
+++ program364.js
@@ -0,0 +1,11 @@
+ 
+function main()
+{
+   let obj = { Name :"Sakshi" , Salary : 21000};
+
+   console.log(obj);
+   console.log(typeof(obj));
+
+}
+
+main();
---
+++ program365.js
@@ -0,0 +1,12 @@
+ 
+function main()
+{
+   let obj = { Name :"Sakshi" , Salary : 21000};
+
+   console.log(obj.Name);
+   console.log(obj.Salary);
+
+
+}
+
+main();
